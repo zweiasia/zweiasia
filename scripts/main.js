d9b2f25b5b56e8b101347ca38bea76750feae965
zweiasia <157432888+zweiasia@users.noreply.github.com>
2024-01-30 15:48:50 +0800

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,15 +18,6 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
-
-
 // Toggle between showing and hiding the sidebar when clicking the menu icon
 var mySidebar = document.getElementById("mySidebar");
 
